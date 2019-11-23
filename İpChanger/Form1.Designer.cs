1c01150258b345e368394a0edadb5b15a412e9e4
ebubekirbastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-11-23 23:40:01 +0200

    Add project files.

---
+++ İpChanger/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace İpChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ribbonControl1 = new DevComponents.DotNetBar.RibbonControl();
+            this.ribbonTabItem1 = new DevComponents.DotNetBar.RibbonTabItem();
+            this.ribbonPanel1 = new DevComponents.DotNetBar.RibbonPanel();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.ipadrs = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Gateway = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.label2 = new System.Windows.Forms.Label();
+            this.subnet = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxEx1 = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.ribbonTabItem2 = new DevComponents.DotNetBar.RibbonTabItem();
+            this.ribbonPanel2 = new DevComponents.DotNetBar.RibbonPanel();
+            this.dns2 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dns1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.ribbonControl1.SuspendLayout();
+            this.ribbonPanel1.SuspendLayout();
+            this.ribbonPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.CaptionVisible = true;
+            this.ribbonControl1.Controls.Add(this.ribbonPanel1);
+            this.ribbonControl1.Controls.Add(this.ribbonPanel2);
+            this.ribbonControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ribbonControl1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.ribbonTabItem1,
+            this.ribbonTabItem2});
+            this.ribbonControl1.KeyTipsFont = new System.Drawing.Font("Tahoma", 7F);
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
+            this.ribbonControl1.Size = new System.Drawing.Size(369, 290);
+            this.ribbonControl1.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.ribbonControl1.TabGroupHeight = 14;
+            this.ribbonControl1.TabIndex = 0;
+            // 
+            // ribbonTabItem1
+            // 
+            this.ribbonTabItem1.Checked = true;
+            this.ribbonTabItem1.ImagePaddingHorizontal = 8;
+            this.ribbonTabItem1.Name = "ribbonTabItem1";
+            this.ribbonTabItem1.Panel = this.ribbonPanel1;
+            this.ribbonTabItem1.Text = "İp Değiştirme";
+            // 
+            // ribbonPanel1
+            // 
+            this.ribbonPanel1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.ribbonPanel1.Controls.Add(this.comboBoxEx1);
+            this.ribbonPanel1.Controls.Add(this.label4);
+            this.ribbonPanel1.Controls.Add(this.subnet);
+            this.ribbonPanel1.Controls.Add(this.label3);
+            this.ribbonPanel1.Controls.Add(this.Gateway);
+            this.ribbonPanel1.Controls.Add(this.label2);
+            this.ribbonPanel1.Controls.Add(this.ipadrs);
+            this.ribbonPanel1.Controls.Add(this.label1);
+            this.ribbonPanel1.Controls.Add(this.buttonX1);
+            this.ribbonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ribbonPanel1.Location = new System.Drawing.Point(0, 55);
+            this.ribbonPanel1.Name = "ribbonPanel1";
+            this.ribbonPanel1.Padding = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.ribbonPanel1.Size = new System.Drawing.Size(369, 233);
+            this.ribbonPanel1.TabIndex = 1;
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(110, 154);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(121, 39);
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "Değiştir";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // ipadrs
+            // 
+            // 
+            // 
+            // 
+            this.ipadrs.Border.Class = "TextBoxBorder";
+            this.ipadrs.Location = new System.Drawing.Point(41, 106);
+            this.ipadrs.Name = "ipadrs";
+            this.ipadrs.Size = new System.Drawing.Size(121, 20);
+            this.ipadrs.TabIndex = 4;
+            this.ipadrs.Text = "192.168.1.20";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(62, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "İp Adresi";
+            // 
+            // Gateway
+            // 
+            // 
+            // 
+            // 
+            this.Gateway.Border.Class = "TextBoxBorder";
+            this.Gateway.Location = new System.Drawing.Point(203, 56);
+            this.Gateway.Name = "Gateway";
+            this.Gateway.Size = new System.Drawing.Size(121, 20);
+            this.Gateway.TabIndex = 6;
+            this.Gateway.Text = "192.168.1.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(222, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Gateway";
+            // 
+            // subnet
+            // 
+            // 
+            // 
+            // 
+            this.subnet.Border.Class = "TextBoxBorder";
+            this.subnet.Location = new System.Drawing.Point(203, 106);
+            this.subnet.Name = "subnet";
+            this.subnet.Size = new System.Drawing.Size(121, 20);
+            this.subnet.TabIndex = 8;
+            this.subnet.Text = "255.255.255.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(212, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subnet Mask";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(71, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Arayüz";
+            // 
+            // comboBoxEx1
+            // 
+            this.comboBoxEx1.DisplayMember = "Text";
+            this.comboBoxEx1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBoxEx1.FormattingEnabled = true;
+            this.comboBoxEx1.ItemHeight = 14;
+            this.comboBoxEx1.Location = new System.Drawing.Point(41, 56);
+            this.comboBoxEx1.Name = "comboBoxEx1";
+            this.comboBoxEx1.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxEx1.TabIndex = 10;
+            // 
+            // ribbonTabItem2
+            // 
+            this.ribbonTabItem2.ImagePaddingHorizontal = 8;
+            this.ribbonTabItem2.Name = "ribbonTabItem2";
+            this.ribbonTabItem2.Panel = this.ribbonPanel2;
+            this.ribbonTabItem2.Text = "Dns Değiştir";
+            // 
+            // ribbonPanel2
+            // 
+            this.ribbonPanel2.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.ribbonPanel2.Controls.Add(this.dns2);
+            this.ribbonPanel2.Controls.Add(this.label5);
+            this.ribbonPanel2.Controls.Add(this.dns1);
+            this.ribbonPanel2.Controls.Add(this.label6);
+            this.ribbonPanel2.Controls.Add(this.buttonX2);
+            this.ribbonPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ribbonPanel2.Location = new System.Drawing.Point(0, 55);
+            this.ribbonPanel2.Name = "ribbonPanel2";
+            this.ribbonPanel2.Padding = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.ribbonPanel2.Size = new System.Drawing.Size(369, 233);
+            this.ribbonPanel2.TabIndex = 2;
+            this.ribbonPanel2.Visible = false;
+            // 
+            // dns2
+            // 
+            // 
+            // 
+            // 
+            this.dns2.Border.Class = "TextBoxBorder";
+            this.dns2.Location = new System.Drawing.Point(190, 56);
+            this.dns2.Name = "dns2";
+            this.dns2.Size = new System.Drawing.Size(121, 20);
+            this.dns2.TabIndex = 13;
+            this.dns2.Text = "208.67.222.222";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(199, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Dns Adresi(2)";
+            // 
+            // dns1
+            // 
+            // 
+            // 
+            // 
+            this.dns1.Border.Class = "TextBoxBorder";
+            this.dns1.Location = new System.Drawing.Point(41, 56);
+            this.dns1.Name = "dns1";
+            this.dns1.Size = new System.Drawing.Size(121, 20);
+            this.dns1.TabIndex = 11;
+            this.dns1.Text = "208.67.220.220";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(60, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Dns Adresi(1)";
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(107, 120);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(121, 39);
+            this.buttonX2.TabIndex = 9;
+            this.buttonX2.Text = "Değiştir";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 290);
+            this.Controls.Add(this.ribbonControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İp Değiştirme Programı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ribbonControl1.ResumeLayout(false);
+            this.ribbonControl1.PerformLayout();
+            this.ribbonPanel1.ResumeLayout(false);
+            this.ribbonPanel1.PerformLayout();
+            this.ribbonPanel2.ResumeLayout(false);
+            this.ribbonPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.RibbonControl ribbonControl1;
+        private DevComponents.DotNetBar.RibbonPanel ribbonPanel1;
+        private DevComponents.DotNetBar.RibbonTabItem ribbonTabItem1;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX subnet;
+        private System.Windows.Forms.Label label3;
+        private DevComponents.DotNetBar.Controls.TextBoxX Gateway;
+        private System.Windows.Forms.Label label2;
+        private DevComponents.DotNetBar.Controls.TextBoxX ipadrs;
+        private System.Windows.Forms.Label label1;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx comboBoxEx1;
+        private System.Windows.Forms.Label label4;
+        private DevComponents.DotNetBar.RibbonPanel ribbonPanel2;
+        private DevComponents.DotNetBar.RibbonTabItem ribbonTabItem2;
+        private DevComponents.DotNetBar.Controls.TextBoxX dns2;
+        private System.Windows.Forms.Label label5;
+        private DevComponents.DotNetBar.Controls.TextBoxX dns1;
+        private System.Windows.Forms.Label label6;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+    }
+}
+
---
+++ İpChanger/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace İpChanger
+{
+    public partial class Form1 : Form
+    {
+        private string nic;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ManagementClass adapterConfig = new ManagementClass("Win32_NetworkAdapterConfiguration");
+        private string description;
+
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+
+            var networkCollection = adapterConfig.GetInstances();
+            foreach (ManagementObject adapter in networkCollection)
+            {
+                description = adapter["Description"].ToString();
+                if (description == comboBoxEx1.Text)
+                {
+                    try
+                    {
+                        // Set DefaultGateway
+                        var newGateway = adapter.GetMethodParameters("SetGateways");
+                        newGateway["DefaultIPGateway"] = new string[] { Gateway.Text };
+                        newGateway["GatewayCostMetric"] = new int[] { 1 };
+
+                        // Set IPAddress and Subnet Mask
+                        var newAddress = adapter.GetMethodParameters("EnableStatic");
+                        newAddress["IPAddress"] = new string[] { ipadrs.Text };
+                        newAddress["SubnetMask"] = new string[] { subnet.Text };
+
+                        adapter.InvokeMethod("EnableStatic", newAddress, null);
+                        adapter.InvokeMethod("SetGateways", newGateway, null);
+
+                        MessageBox.Show("Static İp Adresi Değiştirildi.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("İp Adresi Ayarlanmadı : " + ex.Message);
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var adapterConfig = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            var networkCollection = adapterConfig.GetInstances();
+            foreach (ManagementObject adapter in networkCollection)
+            {
+                comboBoxEx1.Items.Add(adapter["Description"] as string);
+            }
+        }
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            DnsChanger(dns1.Text,dns2.Text);
+            MessageBox.Show("DnsChanger Değiştirdi");
+        }
+        public void DnsChanger(string dns1, string dns2)
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+
+                if ((bool)mo["IPEnabled"])
+                {
+                    nic = mo["Caption"].ToString();
+                    if ((bool)mo["IPEnabled"])
+                    {
+                        if (mo["Caption"].Equals(nic))
+                        {
+                            ManagementBaseObject DnsEntry = mo.GetMethodParameters("SetDNSServerSearchOrder");
+                            string dnsler = dns1 + "," + dns2;
+                            DnsEntry["DNSServerSearchOrder"] = dnsler.Split(',');
+                            ManagementBaseObject DnsMbo = mo.InvokeMethod("SetDNSServerSearchOrder", DnsEntry, null);
+                            int returnCode = int.Parse(DnsMbo["returnvalue"].ToString());
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
